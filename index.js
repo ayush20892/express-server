ae5a5220a047c59e2f98e3bb85f35a19cba43fa4
ayush20892 <ayush20892@gmail.com>
2021-04-21 19:48:37 +0530

    /products

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+// const { appendFile, read } = require('node:fs');
 const app = express();
 const port = 8000;
 
@@ -6,6 +7,19 @@ app.get('/', (req, res) => {
   
   res.send('Hello World! yess')
 });
+
+//Products
+const products = [
+  { name: "apple", color: "red", price: 20},
+  { name: "banana", color: "yellow", price: 20}
+]
+
+app.get('/products', (req, res) => {
+  res.send(products)
+})
+
+
+
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}!`)
