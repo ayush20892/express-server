95b7fea6335e06e94cd9dc51a50733a84c3d2b2d
ayush20892 <ayush20892@gmail.com>
2021-05-13 00:09:15 +0530

    cors updated

--- index.js
+++ index.js
@@ -17,11 +17,24 @@ const port = 8000;
 var productRouter = require('./router/product.router.js');
 var categoryRouter = require('./router/category.router.js')
 
-
+const originlist = [
+  "https://unusual-ecom.netlify.app/",
+  "http://localhost:8000",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+      if (originlist.indexOf(origin) !== -1 || !origin) {
+          callback(null, true);
+      } else {
+          callback(new Error("Not allowed by CORS"));
+      }
+  },
+  optionsSuccessStatus: 200,
+};
 
 initializeDBConnection()
 
-app.use(cors({ "origin": "http://localhost:3000" }));
+app.use(cors(corsOptions));
 
 
 app.get('/', (req, res) => {
