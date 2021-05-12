c37c5aa95652e21b2c21cb4ea5b5d5ed001a766f
ayush20892 <ayush20892@gmail.com>
2021-05-13 00:00:42 +0530

    cors changes

--- index.js
+++ index.js
@@ -17,21 +17,11 @@ const port = 8000;
 var productRouter = require('./router/product.router.js');
 var categoryRouter = require('./router/category.router.js')
 
-var allowlist = ['http://localhost:8000']
-var corsOptions = function (req, callback) {
-  var corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
 
 
 initializeDBConnection()
 
-app.use(cors(corsOptions))
+app.use(cors({ "origin": "http://localhost:3000" }));
 
 
 app.get('/', (req, res) => {
